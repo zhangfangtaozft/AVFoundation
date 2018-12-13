cb5b558f3c771adc44f7da9e47ed7b2f2c2e134e
Frank.zhang <Frank.zhang@neulion.com.cn>
2018-12-13 18:19:18 +0800

    add 01_HelloAVF_Starter

---
+++ 01_HelloAVF_Starter/01_HelloAVF_Starter/BubbleCell.m
@@ -0,0 +1,24 @@
+//
+//  BubbleCell.m
+//  01_HelloAVF_Starter
+//
+//  Created by frank.zhang on 2018/12/13.
+//  Copyright © 2018 Frank.zhang. All rights reserved.
+//
+
+#import "BubbleCell.h"
+
+@implementation BubbleCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ 01_HelloAVF_Starter/01_HelloAVF_Starter/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  01_HelloAVF_Starter
+//
+//  Created by frank.zhang on 2018/12/13.
+//  Copyright © 2018 Frank.zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+@end
+
